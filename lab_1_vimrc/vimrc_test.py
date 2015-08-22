8e538d3fab6fa680c8a08cb7bc240ddedd16932d
Sungchul Choi <sc82.choi@gachon.ac.kr>
2015-08-22 23:29:29 +0900

    Modify vimrc_test.py for handling exstinace of file

--- lab_1_vimrc/vimrc_test.py
+++ lab_1_vimrc/vimrc_test.py
@@ -1,10 +1,19 @@
+from os.path import expanduser
+import os
+
 def get_vimrc_contents():
+	home = expanduser("~")
 	vimrc_contents = ""
+	home_vimrc = os.path.join(home, ".vimrc")
 
-	with open('~/.vimrc', 'r') as f:
-	     vimrc_contents = f.read()
+	if not os.path.exists(home_vimrc):
+		print ("NOT exists: .vimrc")
+		return vimrc_contents
 
-    return vimrc_contents
+	with open(home_vimrc, 'r') as f:
+		vimrc_contents = f.read()
+
+	return vimrc_contents
 
 if __name__ == '__main__':
 	vimrc_contents = get_vimrc_contents()
